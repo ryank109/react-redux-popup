a8d6816da2ddcf2e1baaae993570bafa6586dc7f
Ryan Kim <ryank109@gmail.com>
2016-02-10 17:27:02 -0600

    checking in the lib file

--- webpack.config.js
+++ webpack.config.js
@@ -69,17 +69,17 @@ module.exports = {
       __PRODUCTION__: env.production,
       __CURRENT_ENV__: '\'' + (NODE_ENV) + '\''
     }),
-    // new webpack.optimize.OccurenceOrderPlugin(true),
-    // new webpack.optimize.DedupePlugin(),
-    // new webpack.optimize.UglifyJsPlugin({
-    //   output: {
-    //     comments: false
-    //   },
-    //   compress: {
-    //     warnings: false,
-    //     screw_ie8: true
-    //   }
-    // }),
+    new webpack.optimize.OccurenceOrderPlugin(true),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      output: {
+        comments: false
+      },
+      compress: {
+        warnings: false,
+        screw_ie8: true
+      }
+    }),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify('production')
