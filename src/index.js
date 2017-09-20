92be84486c24e6f08a6f9f9fb89115adefec3bad
Ryan Kim <ryank@6ft.com>
2017-09-19 23:43:32 -0500

    Updated eslint

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-import Modal from 'rrp/modal';
-import Popup from 'rrp/popup';
-import popupReducer from 'rrp/reducer';
-import Portal from 'rrp/portal';
-
 export { closePopup, openPopup, refreshPopupPosition } from 'rrp/actions';
-export {
-    Modal,
-    Popup,
-    popupReducer,
-    Portal
-};
+export { default as Modal } from 'rrp/modal';
+export { default as Popup } from 'rrp/popup';
+export { default as popupReducer } from 'rrp/reducer';
+export { default as Portal } from 'rrp/portal';
