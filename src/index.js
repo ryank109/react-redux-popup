4186e5527ff5eaee8a0159feceffff4f387570b4
Ryan Kim <ryank109@gmail.com>
2018-02-24 17:07:33 -0600

    Emptying out

--- src/index.js
+++
@@ -1,6 +0,0 @@
-export { closePopup, openPopup, refreshPopupPosition } from './actions';
-export { default as Modal } from './modal';
-export { default as Popup } from './popup';
-export { default as popupReducer } from './reducer';
-export { default as Portal, getPortalElement, setPortalElement } from './portal';
-
